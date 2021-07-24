3cc3ee07725e752445819bf290a127556670d2de
ShahidhaNasar3 <86970810+ShahidhaNasar3@users.noreply.github.com>
2021-07-24 21:10:50 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,16 +4,8 @@
 
 class Translation(object):
     
-    Start_Text = """<i>Hey {@Cinemafactory1} @Cinemafactory1 !!</u>
+   START_TEXT = """<a>Hey {@Cinemafactory1} @Cinemafactory1 !!</i>
 @Cinemafactory1 
 <u>"""
-    
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Group Bot Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">@Kaztrouyri</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </
-<b>➥ Source Code</b> : <i><a href="https://github.com/ShahidhaNasar3">Click Me</a></i>
+    ">Click Me</a></i>
 """
